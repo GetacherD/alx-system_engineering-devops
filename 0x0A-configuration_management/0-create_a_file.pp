26d6cc564338e11184a753e7ec2fccfeeeecab67
GetacherD <getacher.d@gmail.com>
2022-09-16 13:40:31 +0300

    first

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 node default {
-file {'tmp/school':
+file {'/tmp':
   ensure  => 'present',
+  path    => '/tmp/school',
   mode    => '0744'
   content => 'I love Puppet',
   owner   => 'www-data',
