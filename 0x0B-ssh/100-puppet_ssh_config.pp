d9bb4ff7ac4fed308af95c907086eb48c8559744
GetacherD <getacher.d@gmail.com>
2022-09-17 09:45:22 -0700

    configure ssh with pupet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+$str = 'Include /etc/ssh/ssh_config.d/*.conf\n
+Host *\n
+    PasswordAuthentication no\n
+    IdentityFile ~/.ssh/school\n
+    SendEnv LANG LC_*\n
+    HashKnownHosts yes\n
+    GSSAPIAuthentication yes\n'
+file {'/etc/ssh/ssh_config':
+    ensure  => 'present',
+    content => $str
+}
