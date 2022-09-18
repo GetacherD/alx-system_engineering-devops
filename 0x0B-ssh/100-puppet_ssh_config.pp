7ac232722b3e899bfd771aaadfafd6ecf1f95df3
GetacherD <getacher.d@gmail.com>
2022-09-18 12:36:49 +0300

    test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,9 @@
-include stdlib
 file_line {'replace_pwd_auth':
-  match   => '*PasswordAuthentication*',
   path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true
+  line    => '    PasswordAuthentication no'
 }
 
 file_line {'replace_identity':
-  match   => '*IdentityFile ~/.ssh/id_rsa*',
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  replace => true
+  line    => '    IdentityFile ~/.ssh/school'
 }
