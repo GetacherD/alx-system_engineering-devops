43e8fd5d7a563f116c1f70bed547f87bedde93da
GetacherD <getacher.d@gmail.com>
2022-09-18 13:01:47 +0300

    add comment

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,12 @@
+# disable pass auth and add identity 
 file_line {'replace_pwd_auth':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no'
 }
 
 file_line {'replace_identity':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school'
 }
