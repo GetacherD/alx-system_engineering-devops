351ae5676f26f96855def9f9964b0f5c0a473618
GetacherD <getacher.d@gmail.com>
2022-09-20 21:05:36 +0300

    pupet conf

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,44 @@
+# configure server
+
+package { 'nginx':
+    ensure => 'installed',
+    before => File['error']
+}
+$err = 'Ceci n\'est pas une page'
+$conf = "server {\n
+   listen   80 default_server;\n
+   listen   [::]:80 default_server;\n
+   root     /var/www/html;\n
+   index    index.html index.htm;\n
+   location /redirect_me {\n
+       return 301 https://www.youtube.com;\n
+   }\n
+   error_page 404 /custom_404.html;\n
+   location = /custom_404.html {\n
+        root /var/www/errors/;\n
+        internal;\n
+   }\n     
+}\n
+"
+file {'error':
+  ensure  => 'present',
+  path    => '/var/www/errors/custom_404.html',
+  content => $err,
+  before  => File['index']
+}
+file { 'index':
+  ensure  => 'present',
+  path    => '/var/www/html/index.html',
+  content => 'Hello World!',
+  before  => File['nginx_conf']
+}
+file { 'nginx_conf':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  content => $conf,
+  before  => Exec['restart']
+}
+exec { 'restart':
+  command => '/usr/bin/service nginx restart',
+  after = File['nginx_conf']
+}
