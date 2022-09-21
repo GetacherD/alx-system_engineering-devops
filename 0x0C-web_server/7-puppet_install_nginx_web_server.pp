2fbe7b43858af355ba39c6735f210b43da508fd4
GetacherD <getacher.d@gmail.com>
2022-09-21 22:30:50 +0300

    update config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,47 +1,41 @@
-# configure server
+# configure new Server
 
 package { 'nginx':
-    ensure => 'installed',
-    before => File['error']
+  ensure => 'installed',
 }
-$err = "Ceci n'est pas une page"
 $conf = "server {
-   listen   80 default_server;
-   listen   [::]:80 default_server;
-   root     /var/www/html;
-   index    index.html index.htm;
-   location /redirect_me {
-       return 301 https://www.youtube.com;
-   }
-   error_page 404 /custom_404.html;
-   location = /custom_404.html {
-        root /var/www/errors/;
-        internal;
-   }
+	listen   80 default_server;
+	listen   [::]:80 default_server;
+	root     /var/www/html;
+	index    index.html index.htm;
+	location /redirect_me {
+		return 301 https://www.youtube.com;
+	}
+	error_page 404 /custom_404.html;
+	location = /custom_404.html {
+		root /var/www/errors/;
+		internal;
+	}
+		
 }
 "
-$hello = "Hello World!"
-file {'error':
+
+file {'/etc/nginx/sites-available/default':
   ensure  => 'present',
-  path    => '/var/www/errors/custom_404.html',
-  content => $err,
-  before  => File['index']
-}
-file { 'index':
-  ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => $hello,
-  before  => File['nginx_conf']
-}
-file { 'nginx_conf':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  content => $conf,
-  before  => Exec['restart']
-}
-exec { 'restart':
-  path    => ['/usr/bin/', '/usr/sbin/'],
-  command => 'service nginx restart',
-  after   => File['nginx_conf']
+  content => $conf
 }
 
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!'
+}
+
+file { '/var/www/errors/custom_404.html':
+  ensure  => 'present',
+  content => "Ceci n\'est pas une page"
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
