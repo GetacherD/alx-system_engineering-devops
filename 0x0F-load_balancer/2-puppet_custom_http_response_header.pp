7fcf2606fcbb8f21f26e55bc1c30eff0cb8dcafb
GetacherD <getacher.d@gmail.com>
2022-09-26 07:32:03 -0700

    use puppet to configure header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,44 @@
+# configure new Server
+
+package { 'nginx':
+  ensure => 'installed',
+}
+$conf = "server {
+	listen   80 default_server;
+	listen   [::]:80 default_server;
+	root     /var/www/html;
+	index    index.html index.htm;
+	location /redirect_me {
+		return 301 https://www.youtube.com;
+	}
+	error_page 404 /custom_404.html;
+	location = /custom_404.html {
+		root /var/www/errors/;
+		internal;
+	}
+	location / {
+		add_header X-Served-By "14407-web-02";
+}
+		
+}
+"
+
+file {'/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $conf
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!'
+}
+
+file { '/var/www/errors/custom_404.html':
+  ensure  => 'present',
+  content => "Ceci n\'est pas une page"
+}
+
+service {'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
