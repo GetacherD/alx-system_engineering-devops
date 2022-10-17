9fb22d7cd2621aee0f6f5a576f233d2c78f820a0
GetacherD <getacher.d@gmail.com>
2022-10-17 19:56:54 +0300

    test

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -12,21 +12,19 @@ if __name__ == "__main__":
         userId = int(sys.argv[1])
     except (IndexError, TypeError, ValueError):
         sys.exit(0)
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-        str(userId))).json()
+    user = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            str(userId))).json()
+    print(user)
+    print(user.get("username"))
     completed = 0
     totals = 0
     tasks_complted = []
-    tasks = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+    tasks = requests.get(
+        "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
+            userId)).json()
     with open("{}.json".format(userId), "w", encoding="utf-8") as f:
-        ts = []
-        for task in tasks:
-            if task.get("userId") == int(sys.argv[1]):
-                d = {}
-                d["task"] = task.get("title")
-                d["completed"] = task.get("completed")
-                d["usename"] = user.get("username")
-                ts.append(d)
-        js = {}
-        js[userId] = ts
-        json.dump(js, f)
+        json.dump({userId: [{"task": task.get("title"),
+                             "completed": task.get("completed"),
+                             "usename": user.get("username")}
+                            for task in tasks]}, f)
