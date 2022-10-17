83002c47e30467fe68aa427efee988ac24d2027c
GetacherD <getacher.d@gmail.com>
2022-10-17 20:02:25 +0300

    test

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -15,16 +15,12 @@ if __name__ == "__main__":
     user = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(
             str(userId))).json()
-    print(user)
-    print(user.get("username"))
-    completed = 0
-    totals = 0
-    tasks_complted = []
+    username = user.get("username")
     tasks = requests.get(
         "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
             userId)).json()
     with open("{}.json".format(userId), "w", encoding="utf-8") as f:
         json.dump({userId: [{"task": task.get("title"),
                              "completed": task.get("completed"),
-                             "usename": user.get("username")}
+                             "usename": username}
                             for task in tasks]}, f)
