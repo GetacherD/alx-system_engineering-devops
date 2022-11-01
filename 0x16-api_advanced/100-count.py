b1eaa6f658ab757c2780474ff9a5aa101ddf28fa
GetacherD <getacher.d@gmail.com>
2022-11-01 18:43:09 +0300

    test

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,7 +6,7 @@ Get title of top ten hot posts
 import requests
 
 
-def count_words(subreddit, word_list=[], hot_list=[], after=""):
+def count_words(subreddit, word_list=[], hot_list=[], after="", res={}):
     """ Recursively get data """
     url = "https://www.reddit.com/r/{}/hot.json?limit=100&after={}".format(
         subreddit, after)
@@ -16,23 +16,17 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
         return
     after = resp.json().get("data").get("after")
     ch = resp.json().get("data").get("children")
+    keys = set([k.lower() for k in word_list])
     for item in ch:
-        hot_list.append(item.get("data").get("title"))
-    if after:
-        return count_words(subreddit, word_list, hot_list, after)
-    try:
-        hot = [h.lower() for h in hot_list]
-        res = {}
-        keys = set([k.lower() for k in word_list])
-        for i in keys:
-            res[i] = 0
-        for h in hot:
-            for key in res:
-                title = h.split(" ")
-                res[key] = res[key] + title.count(key)
-        res = dict(sorted(
-            res.items(), key=lambda x: (-x[1], x[0])))
+        title = item.get("data").get("title").lower()
+        for k in keys:
+            if res.get(k):
+                res[k] += title.split().count(k)
+            else:
+                res[k] = title.split().count(k)
+    if not after:
         i = 0
+        res = dict(sorted(res.items(), key=lambda x: (-x[1], x[0])))
         for key, value in res.items():
             if value:
                 if i == 0:
@@ -43,6 +37,4 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
         if (i == 1):
             print("")
         return
-    except Exception:
-        print("", end="")
-        return
+    return count_words(subreddit, word_list, hot_list, after, res)
