2da5138d72a80ee42ad27dfc3d953e78a216d620
GetacherD <getacher.d@gmail.com>
2022-11-01 18:26:34 +0300

    fix new line

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -13,6 +13,7 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
     headers = {"User-Agent": "Getacher-Top-Ten"}
     resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code != 200:
+        print("", end="")
         return
     after = resp.json().get("data").get("after")
     ch = resp.json().get("data").get("children")
@@ -20,18 +21,31 @@ def count_words(subreddit, word_list=[], hot_list=[], after=""):
         hot_list.append(item.get("data").get("title"))
     if after:
         return count_words(subreddit, word_list, hot_list, after)
-    hot = [hot.lower() for hot in hot_list]
-    res = {}
-    keys = set([k.lower() for k in word_list])
-    for i in keys:
-        res[i] = 0
-    for h in hot:
-        for key in res:
-            title = h.lower().split(" ")
-            res[key] = res[key] + title.count(key)
-    res = dict(sorted(
-        res.items(), key=lambda x: (-x[1], x[0])))
-    for key, value in res.items():
-        if value:
-            print("{}: {}".format(key, value))
-    return
+    try:
+        hot = [h.lower() for h in hot_list]
+        res = {}
+        keys = set([k.lower() for k in word_list])
+        for i in keys:
+            res[i] = 0
+        for h in hot:
+            for key in res:
+                title = h.split(" ")
+                res[key] = res[key] + title.count(key)
+        res = dict(sorted(
+            res.items(), key=lambda x: (-x[1], x[0])))
+        i = 0
+        for key, value in res.items():
+            if value:
+                if i == 0:
+                    print("{}: {}".format(key, value), end="")
+                else:
+                    print("\n{}: {}".format(key, value), end="")
+            i = i + 1
+        if (i == 1):
+            print("")
+        if not res:
+            print("", end="")
+        return
+    except Exception:
+        print("", end="")
+        return
